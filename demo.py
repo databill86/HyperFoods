566b9fe44a0a411372e4cbd215d27a66688593e1
Luís Rita <luis20dr@gmail.com>
2020-02-09 02:18:44 +0000

    Exp13

--- demo.py
+++ demo.py
@@ -5,16 +5,16 @@
 
 import torch
 import os
-# from src.args import get_parser
+from src.args import get_parser
 import pickle
 from src.model import get_model
-# from torchvision import transforms
-# from src.utils.output_utils import prepare_output
-# from PIL import Image
+from torchvision import transforms
+from src.utils.output_utils import prepare_output
+from PIL import Image
 import time
 import sys
 
-# import requests
+import requests
 from io import BytesIO
 
 urls = []
