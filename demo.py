50eefc59fb28c5370a221aba99d8a664030434d7
Luís Rita <luis20dr@gmail.com>
2020-02-10 14:27:11 +0000

    Exp13

--- demo.py
+++ demo.py
@@ -23,15 +23,13 @@ ids = []
 ids.append("hello")
 urls.append(sys.argv[1])
 
-print(os.path.exists('./data/' + 'modelbest.ckpt'))
-
 # print(urls)
-sys.stdout.flush()
+# sys.stdout.flush()
 # print(ids)
 # print(sys.argv[1])
 
 # -----------------------------------------
-'''
+
 data_dir = './data/'
 
 # code will run in gpu if available and if the flag is set to True, else it will run on cpu
@@ -66,7 +64,9 @@ args.ingrs_only = False
 model = get_model(args, ingr_vocab_size, instrs_vocab_size)
 # Load the trained model parameters
 model_path = os.path.join(data_dir, 'modelbest.ckpt')
-# print(os.path.exists(data_dir + 'modelbest.ckpt'))
+print(os.path.exists('./data/' + 'modelbest.ckpt'))
+sys.stdout.flush()
+'''
 # model_path = os.path.join("https://drive.google.com/open?id=1dpSIUZFtl03dvH1midHn67EZKyDzEAvy", "")
 model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu'))) # map_location=map_loc
 
